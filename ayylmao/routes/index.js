73743caf1f50ce79210317d624285f34197ad437
Christopher Gregorian <csgregorian@gmail.com>
2016-01-24 06:51:05 -0500

    1

--- ayylmao/routes/index.js
+++ ayylmao/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 // var twilio = require("twilio")("SKe123538ec0d05993e10b7ac024154f4f", "OARuYI2Cg9oDOwi938tudwuCC6tuCF3m");
 var twilio = require("twilio");
 var http = require("http");
-var io = require('socket.io')(http);
+var io = require('socket.io')(80);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
